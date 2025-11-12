25c62d9afb8dc2636c49aa9e57f59020665f178f
Rudrax Patel <prudrax97@gmail.com>
2025-11-12 11:53:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ OWNER_ID = int(os.getenv("OWNER_ID"))
 ADMINS = [int(x) for x in os.getenv("ADMINS", "").split(",")]
 
 MONGO_URL = os.getenv("DATABASE_URL") 
-
+MONGO_DB = os.getenv("")
 GROUP_URL = os.getenv("GROUP_URL")
 CHANNEL_URL = os.getenv("CHANNEL_URL")
 SUPPORT_GROUP_URL = os.getenv("SUPPORT_GROUP_URL")
